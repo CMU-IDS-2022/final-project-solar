7fb67e83f3170019d66ce3fc55c9232e177c9321
jamiehcw <jamieh@andrew.cmu.edu>
2022-04-26 01:31:26 -0400

    Add files via upload

---
+++ TableData.py
@@ -0,0 +1,74 @@
+TABLENONE = '''
+| Name | Title | Established | Requirement | Applicable Sectors | Enabling Statute, Code,  or Order |
+| ---- | ----- | ----------- | ----------- | ------------------ | --------------------------------- |
+| Alabama |  |  |  |  |  |
+| Alaska |  |  |  |  |  |
+| Arkansas |  |  |  |  |  |
+| Florida |  |  |  |  |  |
+| Georgia |  |  |  |  |  |
+| Idaho |  |  |  |  |  |
+| Kentucky |  |  |  |  |  |
+| Louisiana |  |  |  |  |  |
+| Mississippi |  |  |  |  |  |
+| Nebraska |  |  |  |  |  |
+| Tennessee |  |  |  |  |  |
+| West Virginia | Alternative and Renewable Energy Portfolio Standard- REPEALED | 2009; Repealed 2015 | 10% from 2015-2019, 15% from 2020-2024, 25% by 2025 | Investor-owned utility | W. Va. Code §24-2F; Repeal: House Bill 2001 |
+| Wyoming |  |  |  |  |  |
+'''
+
+
+TABLEREQUIRED = '''
+| Name | Title | Established | Requirement | Applicable Sectors | Enabling Statute, Code,  or Order |
+| ---- | ----- | ----------- | ----------- | ------------------ | --------------------------------- |
+| Arizona| Renewable Energy Standard | 2006 | 15% by 2025 | Investor-owned utility, retail supplier | Ariz. Admin. Code §14-2-1801 et seq. |
+| California | Renewables Portfolio Standard | 2002 | 44% by 2024; 52%  by 2027; 60% by 2030. Also requires 100% clean energy by 2045 | Investor-owned utility,  municipal utilities | Cal. Public Utilities Code §399.11 et seq.; Cal. Public Resources Code §25740 et seq.; Assembly Bill 327 (2013); Senate Bill 350 (2015); Senate Bill 100 (2018) |
+| Colorado | Renewable Energy Standard | 2004 | 30% by 2020 (IOUs); 10% or 20% for municipalities and electric cooperatives depending on size; 100% clean energy by 2050 for utilities serving 500,000 or more customers | Investor owned utility, municipal utilities, cooperative utilities | Colo. Rev. Stat. §40-2-124; Senate Bill 252 (2013); Senate Bill 263 (2019) |
+| Connecticut | Renewables Portfolio Standard | 1998 | 44% by 2030 | Investor-owned utility, local government, retail supplier | Conn. Gen. Stat. §16-245a et seq.; Conn. Gen. Stat. §16-1; Senate Bill 9 (2018) |
+| Delaware | Renewable Energy Portfolio Standard | 2005 | In 2021 the state extended its RPS to include long-term targets of 28% by 2030 and 40% by 2035. The changes retained the state's previous target for 2025 of 25%.  | Investor-owned utility, local government, retail supplier | Del. Code Ann. 26 §351 et seq.; Senate Bill 33 (2021). |
+| District of Columbia | Renewable Portfolio Standard | 2005 | 20% by 2020, 100% by 2032 | Investor-owned utility, retail supplier | D.C. Code §34-1431 et seq., Bill 650 (2016); Bill 904 (2018) |
+| Hawaii | Renewable Portfolio Standard | 2001 | 30% by 2020; 40% by 2030; 70% by 2040; 100% by 2045 | Investor-owned utility | Hawaii Rev. Stat. §269-91 et seq.; House Bill 623 (2015) |
+| Illinois | Renewable Portfolio Standard | 2001 (voluntary target); 2007 (standard) | 25% by 2025-2026 | Investor-owned utility, retail supplier | Ill. Rev. Stat. ch. 20 §688 (2001); Ill. Rev. Stat. ch. 20 §3855/1-75 (2007); Senate Bill 2814 (2016) |
+| Maine | Renewables Portfolio Standard | 1999 | 80% by 2030; statewide target of 100% renewables by 2050 | Investor-owned utility, retail supplier | Me. Rev. Stat. Ann. 35-A §3210 et seq.; Me. Rev. Stat. Ann. 35-A §3401 et seq. (wind energy); Senate File 457 (2019) |
+| Maryland | Renewable Energy Portfolio Standard | 2004 | 30.5% in 2020; 50% in 2030 | Investor-owned utility, local government, retail supplier | Md. Public Utilities Code Ann. §7-701 et seq.; Senate Bill 516 (2019) |
+| Massachusetts | Renewable Portfolio Standard | 1997 | Class I: 35% by 2030 and an additional 1% each year after. Class II: 6.7% by 2020 | Investor-owned utility, retail supplier | Mass. Gen. Laws Ann. ch. 25A §11F; House Bill 4857 (2018) |
+| Michigan | Renewable Energy Standard | 2008; 2016 | 15% by 2021 (standard), 35% by 2025 (goal, including energy efficiency and demand reduction) | Investor-owned utility, municipal utilities, cooperative utilities, retail supplier | Mich. Comp. Laws §460.1001 et seq.; Senate Bill 438 (2016) |
+| Minnesota | Renewables Energy Standard | 2007 | 26.5% by 2025 (IOUs) |  25% by 2025 (other utilities) | Investor-owned utility, municipal utilities, cooperative utilities | Minn. Stat. §216B.1691 |
+| Missouri | Renewable Electricity Standard | 2007 | 15% by 2021 (IOUs) | Investor-owned utility | Mo. Rev. Stat. §393.1020 et seq. |
+| Nevada | Energy Portfolio Standard | 1997 | 50% by 2030; non-binding 100% carbon-free by 2050 | Investor-owned utility, retail supplier | Nev. Rev. Stat. §704.7801 et seq.; Senate Bill 358 (2019) |
+| New Hampshire | Electric Renewable Portfolio Standard | 2007 | 25.2% by 2025 | Investor-owned utility, cooperative utilities, retail supplier | N.H. Rev. Stat. Ann. §362-F |
+| New Jersey | Renewables Portfolio Standard | 1991 | 50% by 2030 | Investor-owned utility, retail supplier | N.J. Rev. Stat. §48:3-49 et seq.; Assembly Bill 3723 (2018) |
+| New Mexico | Renewables Portfolio Standard | 2002 | 40% by 2025; 80% renewables by 2040; 100% of electricity supplied by zero-carbon resources by 2045 | Investor-owned utility, cooperative utilities | N.M. Stat. Ann. §62-15-1 et seq.; N.M. Stat. Ann. §62-16-1 et seq.; Senate Bill 489 (2019) |
+| New York | Renewable Portfolio Standard; Reforming the Energy Vision (REV) | 2004 | 70% renewables by 2030; 100% zero-emissions electricity requirement by 2040 | Investor-owned utility, municipal utilities, cooperative utilities, retail supplier | NY PSC Order Case 03-E-0188; 2015 New York State Energy Plan; Senate Bill 6599 (2019) |
+| North Carolina | Renewable Energy and Energy Efficiency Portfolio Standard | 2007 | 12.5% by 2021 (IOUs); 10% by 2018 (munis and coops) | Investor-owned utility, municipal utilities, cooperative utilities | N.C. Gen. Stat. §62-133.8 |
+| North Dakota | Renewable and Recycled Energy Objective | 2007 | 10% by 2015 | Investor-owned utility, municipal utilities, cooperative utilities | N.D. Cent. Code §49-02-24 et seq. |
+| Ohio | Alternative Energy Resource Standard | 2008 | 8.5% by 2026 | Investor-owned utility, retail supplier | Ohio Rev. Code Ann. §4928.64 et seq.; House Bill 6 (2019) |
+| Oregon | Renewable Portfolio Standard | 2007 | In July 2021, Oregon passed legislation to complement its RPS. The state’s “Clean Energy Targets” legislation requires retail electricity providers to reduce emissions by the following levels below baseline: 80% by 2030; 90% by 2035; and 100% by 2040 (see below for further explanation. | Investor-owned utility, municipal utilities, cooperative utilities, retail supplier | Or. Rev. Stat. §469a; Senate Bill 1547 (2016) |
+| Pennsylvania | Alternative Energy Portfolio Standard | 2004 | 18% by 2020-2021 | Investor-owned utility, retail supplier | Pa. Cons. Stat. tit. 66 §2814 |
+| Rhode Island | Renewable Energy Standard | 2004 | 14.5% by 2019, with increases of 1.5% each year until 38.5% by 2035 | Investor-owned utility, retail supplier | R.I. Gen. Laws §39-26-1 et seq.; R.I. Gen. Laws §39-26.1 et seq. (contracting standard); House Bill 7413a (2016) |
+| Texas | Renewable Generation Requirement | 1999 | 5,880 MW by 2015. 10,000 MW by 2025 (goal; achieved) | Investor-owned utility, retail supplier | Tex. Utilities Code Ann. §39.904. |
+| Vermont | Renewable Portfolio Standard | 2005 (voluntary target); 2015 (standard) | 55% by 2017; 75% by 2032 | Investor-owned utility, municipal utilities, cooperative utilities, retail supplier | Vt. Stat. Ann. tit. 30 §8001 et seq.; Standard: House Bill 40 |
+| Virginia | Renewable Energy Standard | 2020 | 100% renewables by 2045 for Phase II utilities and 2050 for Phase I utilities | Investor-owned utility | Va. Code §56-585.2; Senate Bill 851 (2020) |
+| Washington | Renewable Energy Standard | 2006 | 15% renewable by 2020; 100% greenhouse gas neutral by 2030; 100% renewable or zero-emitting by 2045 | Investor-owned utility, municipal utilities, cooperative utilities | Wash. Rev. Code §19.285; Wash. Admin. Code §480-109; Wash Admin. Code §194-37; Senate Bill 5116 (2019) |
+'''
+
+
+TABLEEXPIRED = '''
+| Name | Title | Established | Requirement | Applicable Sectors | Enabling Statute, Code,  or Order |
+| ---- | ----- | ----------- | ----------- | ------------------ | --------------------------------- |
+| Iowa | Alternative Energy Law | 1983 | 105 MW of generating capacity for IOUs. | Investor-owned utility | Iowa Code §476.41 et seq. |
+| Kansas | Renewable Energy Goal | 2009 (standard); 2015 (goal) | 15% by 2015-2019; 20% by 2020 | Investor-owned utility | Kan Stat. Ann. §66-1256 et seq.; Goal: Senate Bill 91 |
+| Montana | Renewable Resource Standard | 2005 | 15% by 2015 | Investor-owned utility, retail supplier | Mont. Code Ann. §69-3-2001 et seq. |
+| Oklahoma | Renewable Energy Goal | 2010 | 15% by 2015 | Investor-owned utility, municipal utilities, cooperative utilities | Okla. Stat. tit. 17 §801.1 et seq. |
+| South Dakota | Renewable, Recycled and Conserved Energy Objective | 2008 | 10% by 2015 | Investor-owned utility, municipal utilities, cooperative utilities | S.D. Codified Laws Ann. §49-34A-94; S.D. Codified Laws Ann. §49-34A-101 et seq. |
+| Wisconsin | Renewable Portfolio Standard | 1998 | 10% by 2015 | Investor-owned utility, municipal utilities, cooperative utilities | Wisc. Stat. §196.378 |
+'''
+
+
+
+TABLEVOLUNTARY = '''
+| Name | Title | Established | Requirement | Applicable Sectors | Enabling Statute, Code,  or Order |
+| ---- | ----- | ----------- | ----------- | ------------------ | --------------------------------- |
+| Indiana | Clean Energy Portfolio Goal | 2011 | 10% by 2025 | Investor-owned utility, municipal utilities, cooperative utilities, retail supplier | Ind. Code §8-1-37. |
+| South Carolina | Voluntary Renewables Portfolio Standard | 2014 | 2% by 2021 | Investor-owned utility | House Bill 1189 |
+| Utah | Renewables Portfolio Goal | 2008 | 20% by 2025 | Investor-owned utility, municipal utilities, cooperative utilities | Utah Code Ann. §54-17-101 et seq.; Utah Code Ann. §10-19-101 et seq. |
+'''
